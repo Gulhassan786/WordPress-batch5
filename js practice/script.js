dd46cb8607a356fb869e66165ea25599cb7ec2cf
gulhassan786 <gulhassanh49@gmail.com>
2024-09-23 22:59:29 +0500

    added js code

---
+++ js practice/script.js
@@ -0,0 +1,52 @@
+
+// alert("This is inner js!")
+
+// output methods
+
+console.log("This is console output")
+
+document.write("This is document method to show output!")
+
+// Varialbes
+
+let name = "Gul Hassan"
+
+let age = 50
+
+var cl = 12
+
+var s = age + cl
+
+console.log(s)
+// document.write(name)
+console.log(typeof(age))
+
+const bs = "IT"
+const PI = 3.14
+// bs = "gul hassan"
+// PI = 4.14
+
+// Array object
+
+let pets = ["Cat", "Horse", "bird"];
+console.log(pets[1])
+console.log(typeof(pets))
+
+// pets [0]
+
+// object {key: value, key: value}
+// hassan
+
+let dict = {"name":"Gul Hassan", "age":23, "course":"wordpress"}
+
+/* console.log(dict)
+console.log(dict["course"])
+console.log(dict["age"])
+
+*/
+console.log(age + cl)
+console.log(2*2) // x
+console.log(4/2)
+console.log(8-5)
+console.log(7%2)
+
